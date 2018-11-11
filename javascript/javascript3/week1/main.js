c189561917699181206025caba9725504b845b48
Gerlie Traya <navasquezgerlie@yahoo.com>
2018-11-11 10:07:46 +0100

    added act 2 json file movies get functions

--- javascript/javascript3/week1/main.js
+++ javascript/javascript3/week1/main.js
@@ -5,3 +5,23 @@ let newNumbers = [];
 const oddNumber = numbers.filter(number => number % 2 !== 0).map(number => number * 2);
 // console.log(oddNumber);
 console.log("The doubled numbers are ", oddNumber);
+// Using this json file as the source, build a function which does the following:
+const url = 'https://gist.githubusercontent.com/pankaj28843/08f397fcea7c760a99206bcb0ae8d0a4/raw/02d8bc9ec9a73e463b13c44df77a87255def5ab9/movies.json'
+//getting the movie titles//
+fetch('https://gist.githubusercontent.com/pankaj28843/08f397fcea7c760a99206bcb0ae8d0a4/raw/02d8bc9ec9a73e463b13c44df77a87255def5ab9/movies.json')
+    .then((res) => res.json())
+    .then((movies) => {
+       const movieRatings = movies.map((movie) => movie.rating)
+       console.log(movieRatings)
+    })
+fetch('https://gist.githubusercontent.com/pankaj28843/08f397fcea7c760a99206bcb0ae8d0a4/raw/02d8bc9ec9a73e463b13c44df77a87255def5ab9/movies.json')
+    .then((res) => res.json())
+    .then((movies) => {    
+        const goodMovies = movies.filter(goodMovies => goodMovies.rating >= 7);
+        const averageMovies = movies.filter(averageMovies => averageMovies.rating >= 4 && averageMovies.rating < 7);
+        const badMovies = movies.filter(badMovies => badMovies.rating < 4);
+        console.log(goodMovies);
+        console.log(averageMovies);
+        console.log(badMovies);
+        
+    })
